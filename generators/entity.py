ce616d27f8c616a9dd3f24efb6372d6974460561
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2023-06-05 10:43:40 -0500

    Add preliminary generator specs, refine entities

---
+++ generators/entity.py
@@ -0,0 +1,11 @@
+# IQUIST/NCSA
+# UIUC
+import sys
+
+
+def generate(name, filename):
+    print("Entities do not have direct generators. Aborting.")
+    
+if __name__ == '__main__':
+    generate(sys.argv[1])
+    
