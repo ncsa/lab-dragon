70a986ddef4485a08756f5e4a17a298f891011c4
marcosf2 <marcosf2@illinois.edu>
2023-06-07 13:57:21 -0500

    added task generator

--- generators/task.py
+++ generators/task.py
@@ -6,11 +6,12 @@ from datetime import datetime
 from entity import _generate as generate_entity
 
 __description__ = """
-Generator for a step
+Generator for a task
 =========================
 
-A step reflects an atomic action that was performed by a researcher. This action can be a part of a bigger task.
-Steps can also have instances attached to them to indicate that this step created data.
+A Task reflects a collection (can also be a single one) of steps to perform a certain action.
+Because of this, Tasks have objectives. Tasks can be composed of a collection of individual steps. 
+
 
 Contents:
 * An step TOML file
@@ -18,6 +19,7 @@ Contents:
 """
 def generate(name,
              filename,
+             objective,
              process,
              parent_class,
              *args, **kwargs):
@@ -26,6 +28,7 @@ def generate(name,
     start_time = datetime.now()
     doc["fields"]["start_time"] = start_time.isoformat()
     doc["fields"]["end_time"] = 0
+    doc["fields"]["objective"] = objective
     doc["fields"]["process"] = process
     doc["fields"]["parent_class"] = parent_class
 
@@ -42,6 +45,7 @@ if __name__ == "__main__":
 
     parser.add_argument("name", help="The name of the Step")
     parser.add_argument("filename", help="The name of the file to write the Step to")
+    parser.add_argument("--objective", default="", help="The objective of the Step")
     parser.add_argument("--process", default="", help="The process of the Step")
     parser.add_argument("--parent_class", default="entity", help="The parent class of the Step")
     parser.add_argument("--parent_link", default="", help="The parent link of the Step")
@@ -54,6 +58,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     name = args.name
     filename = args.filename
+    objective = args.objective
     process = args.process
     parent_class = args.parent_class
     parent_link = args.parent_link
@@ -65,6 +70,7 @@ if __name__ == "__main__":
 
     generate(name,
              filename,
+             objective,
              process,
              parent_class,
              parent_link=parent_link,
