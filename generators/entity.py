068499ed1ed5e0ecfa615f50be1d4334baf09047
marcosf2 <marcosf2@illinois.edu>
2023-06-07 11:03:03 -0500

    entity generator

--- generators/entity.py
+++ generators/entity.py
@@ -1,11 +1,37 @@
 # IQUIST/NCSA
 # UIUC
-import sys
+import os
+import uuid
+import tomlkit
+from datetime import datetime
 
+def _generate(name = '',
+             parent_link = '',
+             user = 'default',
+             description = '',
+             related_links = '',
+             params = ''):
 
-def generate(name, filename):
-    print("Entities do not have direct generators. Aborting.")
-    
+    timestamp = datetime.now().isoformat()
+
+    doc = tomlkit.document()
+    fields = tomlkit.table()
+    id = str(uuid.uuid4())
+    fields['id'] = id
+    fields['timestamp'] = timestamp
+    if name != '':
+        fields['name'] = name
+    else:
+        fields['name'] = f'entity_{id}'
+
+    fields['parent_link'] = parent_link
+    fields['user'] = user
+    fields['description'] = description
+    fields['related_links'] = related_links
+    fields['params'] = params
+    doc['fields'] = fields
+
+    return doc
+
 if __name__ == '__main__':
-    generate(sys.argv[1])
-    
+    print(f'Entities do not have direct generators. Aborting.')
