889b7671cf6cca64b23590a0bbbafdb786a86f45
marcosf2 <marcosf2@illinois.edu>
2023-06-23 14:15:19 -0500

    added loading entity from file test

--- test/pytest/test_generator.py
+++ test/pytest/test_generator.py
@@ -1,47 +1,80 @@
-import importlib
+import os
 import time
+import string
+import random
+import importlib
 from pathlib import Path
 
 import tomllib as toml
+from pprint import pprint
 
 import qdata
 import qdata.modules
-from qdata.generators.generator import generate_all_classes
+from qdata.generators.generator import generate_all_classes, read_from_TOML
 
 
-def count_files(path):
-    path = Path(path)
+def generate_random_string(length):
+    characters = string.ascii_lowercase + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
 
-    if path.is_dir():
-        return len([x for x in path.glob('*') if '.DS_Store' not in str(x) or '__pycache__' not in str(x)])
+
+def get_toml_module_and_class(item):
+    toml_path = qdata.SCHEMASDIR.joinpath(f'{item}.toml')
+
+    with open(str(toml_path), 'rb') as f:
+        toml_doc = toml.load(f)
+
+    module = importlib.import_module(f'qdata.modules.{item}')
+    cls = item[0].upper() + item[1:]
+    _class = getattr(module, cls)
+
+    return toml_doc, _class
 
 
-def test_fields_in_generated_class():
+def test_all_fields_in_generated_class(module_names):
 
-    nschemas = count_files(qdata.SCHEMASDIR)
-    nmodules = count_files(qdata.MODULESDIR)
+    classes = module_names
 
-    if nmodules != nschemas:
-        raise FileNotFoundError(f'Number of schemas ({nschemas}) does not match number of modules ({nmodules})')
-
-    classes = [x.stem for x in qdata.MODULESDIR.glob('*.py') if '__init__' not in str(x)]
     for cls in classes:
 
-        toml_path = qdata.SCHEMASDIR.joinpath(f'{cls}.toml')
-
-        with open(str(toml_path), 'rb') as f:
-            toml_doc = toml.load(f)
-
-        module = importlib.import_module(f'qdata.modules.{cls}')
-        cls = cls[0].upper() + cls[1:]
-        _class = getattr(module, cls)
+        toml_doc, _class = get_toml_module_and_class(cls)
 
         instance = _class()
         for field in toml_doc['definitions']:
+            print(f'\n doing field: {field} with val: {toml_doc["definitions"][field]}')
             assert hasattr(instance, field)
 
 
+def test_re_instantiation_of_classes(module_names):
 
+    classes = module_names
 
+    for cls in classes:
+        toml_doc, _class = get_toml_module_and_class(cls)
+
+        fields = {'name': generate_random_string(5)}
+        for field in toml_doc['definitions']:
+            if 'time' in field:
+                continue
+            elif 'ID' in field:
+                continue
+            elif 'link' in field:
+                continue
+            elif 'List[str]' in toml_doc['definitions'][field]:
+                comments = []
+                for x in range(5):
+                    comments.append(generate_random_string(5))
+                fields[field] = comments
+            elif 'str' in toml_doc['definitions'][field]:
+                fields[field] = generate_random_string(8)
+
+        instance = _class(**fields)
+        toml_path = Path(os.getcwd()).joinpath(fields['name'] + '.toml')
+        instance.to_TOML(toml_path)
+        retrieved_instance = read_from_TOML(toml_path)
+        toml_path.unlink(missing_ok=True)
+
+        assert instance == retrieved_instance
 
 
