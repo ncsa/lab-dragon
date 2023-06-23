085e795580ddd4361e0ffb179fef58add3839895
marcosf2 <marcosf2@illinois.edu>
2023-06-23 14:33:32 -0500

    Added docstrings

--- qdata/generators/generator.py
+++ qdata/generators/generator.py
@@ -2,20 +2,32 @@ import importlib
 from datetime import datetime, timezone
 
 from pathlib import Path
-from pprint import pprint
 from typing import Union
-from jinja2 import Template, Environment, FileSystemLoader
+from jinja2 import Template
 import tomllib as toml
 
 from qdata import SCHEMASDIR, MODULESDIR, TEMPLATESDIR
 from qdata import modules
 
-def create_timestamp():
+
+def create_timestamp() -> str:
+    """
+    Creates a timestamp in ISO 8601 format.
+
+    :return: Timestamp in ISO 8601 format.
+    """
     timestamp = datetime.now(timezone.utc).astimezone().isoformat()
     return timestamp
 
 
-def parser(path: Union[Path, str]):
+def parser(path: Union[Path, str]) -> dict:
+    """
+    Parses a TOML file and returns a dictionary of the contents necessary to create a class based on the entity.jinja
+    template.
+
+    :param path: The path to the TOML file to be parsed.
+    :return: Dictionary in which keys are the values that need the jinja template needs.
+    """
     with open(path, 'rb') as f:
         data = toml.load(f)
 
@@ -57,9 +69,15 @@ def parser(path: Union[Path, str]):
     return ret
 
 
+# TODO: If we keep the jinja way of generating code, we need to be able to select custom parsers
 def generate_class(config_path: Union[str, Path],
-                   template_path: Union[str, Path] = TEMPLATESDIR.joinpath("entity.jinja")):
+                   template_path: Union[str, Path] = TEMPLATESDIR.joinpath("entity.jinja")) -> None:
+    """
+    Generates a class using a Jinja template based on the TOML file provided.
 
+    :param config_path: Path to the TOML config file.
+    :param template_path: Path to the jinja template file.
+    """
     with open(template_path, 'r') as f:
         template_content = f.read()
 
@@ -71,13 +89,19 @@ def generate_class(config_path: Union[str, Path],
     with open(str(MODULESDIR.joinpath(f'{vals_dict["class_name"]}.py'.lower())), 'w') as f:
         f.write(output)
 
+
 # TODO: Have error catching this for when there are more than a single item
-def read_from_TOML(path: Union[str, Path]):
+def read_from_TOML(path: Union[str, Path]) -> object:
+    """
+    Reads a TOML file and returns an instantiated object of the class specified in the file.
+
+    :param path: The path to the TOML file.
+    :return: An instantiated object of the class specified in the TOML file.
+    """
     with open(str(path), 'rb') as f:
         data = toml.load(f)
 
     name = [key for key in data.keys()][0]
-
     data = data[name]
 
     module = importlib.import_module(f'qdata.modules.{data["type"].lower()}')
@@ -87,15 +111,20 @@ def read_from_TOML(path: Union[str, Path]):
     return ins
 
 
-def generate_all_classes():
+def generate_all_classes() -> None:
+    """
+    Helper class to create all the classes in the schemas directory.
+    """
     for f in SCHEMASDIR.glob('*.toml'):
         if 'hierarchy.toml' in f.name:
             continue
-
         generate_class(f)
 
 
-def delete_all_modules():
+def delete_all_modules() -> None:
+    """
+     Helper class to delete all modules in the modules directory.
+    """
     for f in MODULESDIR.glob('*.py'):
         if '__init__.py' in f.name:
             continue
