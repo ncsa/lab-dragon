b94c74cca5868f0d9692242b4637bff3fc5389f8
marcosf2 <marcosf2@illinois.edu>
2024-02-19 14:57:13 -0600

    You can now drag the space between the sideabr and content to rezise it

--- app/components/EntityPopupWrapper.jsx
+++ app/components/EntityPopupWrapper.jsx
@@ -1,27 +1,24 @@
 "use client"
 
 import React, { useState, useContext, useEffect } from 'react';
+import SplitPane from 'react-split-pane'; // Assuming you have or will install react-split-pane
+
 
 // components
 import Sidebar from './Sidebar';
 import EntityInput from './EntityInput';
 import { CreationPopupContext } from '../contexts/CreationPopupContext';
 
-
 async function getSidebarItems() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/entities`);
     return await response.json();
 }
 
-
 export default function EntityPopupWrapper({ children }) {
-    
     const [entities, setEntities] = useState([]); // The entities that will be displayed in the sidebar
-    const { isPopupOpen, setIsPopupOpen } = useContext(CreationPopupContext);
-    const { setRootEntity } = useContext(CreationPopupContext);
-    const { setName } = useContext(CreationPopupContext);
+    const { isPopupOpen, setIsPopupOpen, setRootEntity, setName } = useContext(CreationPopupContext);
 
-    const closePopup = () => {setIsPopupOpen(false)}
+    const closePopup = () => { setIsPopupOpen(false) }
 
     function populateSidebarItems() {
         getSidebarItems().then(data => {
@@ -34,28 +31,21 @@ export default function EntityPopupWrapper({ children }) {
         populateSidebarItems();
     }, []);
 
-
     return (
         <div>
             {isPopupOpen ? <EntityInput populateSideBar={populateSidebarItems}/> : null}
-            <div className="flex-container">
+            <SplitPane split="vertical" defaultSize="50%">
                 <div className="sidebar-wrapper">
                     <Sidebar entities={entities}/>
                 </div>
                 <div className="children-wrapper">
                     {children}
                 </div>
-            </div>
+            </SplitPane>
             <button className="add-entity-button" title="Adds new entity to the notebook" onClick={() => {
                     setName("");
                     setIsPopupOpen(!isPopupOpen);
                 }}><i className="bi bi-journal-plus"/></button>
         </div>
-    )
-
-    
+    );
 }
-
-
-
-
