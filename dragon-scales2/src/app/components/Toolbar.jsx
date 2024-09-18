f805049377071bc654e7f46cf1b917d18a178297
Smital-Lunawat <lunawatsmital@gmail.com>
2024-09-17 22:36:32 -0500

    Changes to toolbar UI

--- dragon-scales2/src/app/components/Toolbar.jsx
+++ dragon-scales2/src/app/components/Toolbar.jsx
@@ -6,15 +6,13 @@ import { navLinks } from '../constants';
 import { FaBook, FaCommentDots, FaSearch, FaUserCircle } from 'react-icons/fa';
 import Image from 'next/image';
 
-
 const Toolbar = () => {
     const pathname = usePathname();
 
     return (
         <div className="fixed left-0 top-0 h-full w-16 flex flex-col justify-between bg-grey shadow-lg">
             {/* Logo */}
-            
-                <Link href="/" className="w-full p-3 flex justify-center items-center hover:bg-green">
+            <Link href="/" className={`w-full p-3 flex justify-center items-center ${pathname === '/' ? 'bg-green' : 'hover:bg-green'}`}>
                 <div className="w-10 h-10 rounded-full bg-white shadow flex items-center justify-center ">
                     <Image
                         src="/logo.png"
@@ -24,23 +22,24 @@ const Toolbar = () => {
                         className="object-cover"
                     />
                 </div>
-                </Link>
+            </Link>
 
             {/* Navigation Links */}
-            <div className="flex-grow flex flex-col mt-6">
+            <div className="flex-grow flex flex-col">
                 {navLinks.filter(link => ['library', 'comment', 'search'].includes(link.id)).map((link) => (
                     <Link
                         key={link.id}
                         href={`/${link.id}`}
-                        className={`w-full p-3 flex justify-center items-center hover:bg-blue transition-colors ${pathname === `/${link.id}` 
-                            ? 'bg-blue-500 rounded-r-lg' 
-                            : 'hover:bg-blue-100 rounded-r-lg'
+                        className={`w-full p-3 flex justify-center items-center transition-colors ${
+                            pathname === `/${link.id}`
+                                ? 'bg-blue text-blue rounded-md' // Active state
+                                : 'hover:bg-blue-100 text-gray-600 rounded-md'
                         }`}
                     >
-                        <div className="w-10 h-10 rounded-md bg-white shadow flex items-center justify-center">
-                            {link.id === 'library' && <FaBook className="text-blue text-xl" />}
-                            {link.id === 'comment' && <FaCommentDots className="text-blue text-xl" />}
-                            {link.id === 'search' && <FaSearch className="text-blue text-xl" />}
+                        <div className={`w-10 h-10 rounded-md ${pathname === `/${link.id}` ? 'bg-white' : 'bg-white'} shadow flex items-center justify-center`}>
+                            {link.id === 'library' && <FaBook className={`${pathname === `/${link.id}` ? 'text-blue' : 'text-blue'} text-xl`} />}
+                            {link.id === 'comment' && <FaCommentDots className={`${pathname === `/${link.id}` ? 'text-blue' : 'text-blue'} text-xl`} />}
+                            {link.id === 'search' && <FaSearch className={`${pathname === `/${link.id}` ? 'text-blue' : 'text-blue'} text-xl`} />}
                         </div>
                     </Link>
                 ))}
@@ -49,7 +48,7 @@ const Toolbar = () => {
             {/* Profile */}
             <div className="w-full p-3 flex justify-center items-center">
                 <div className="w-10 h-10 rounded-full bg-white shadow flex items-center justify-center">
-                    <FaUserCircle className="text-blue text-xl" /> {/* Profile icon */}
+                    <FaUserCircle className="text-blue text-xl" />
                 </div>
             </div>
         </div>
