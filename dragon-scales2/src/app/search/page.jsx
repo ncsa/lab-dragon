bd8e6d6d2a85465c36248f09a28c3c2f85f2e621
Smital-Lunawat <lunawatsmital@gmail.com>
2024-10-04 10:42:58 -0500

    Added library pane

--- dragon-scales2/src/app/search/page.jsx
+++ dragon-scales2/src/app/search/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 export default function Search() {
   return (
+    <div className='main-content'>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>Search Page</h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
         </div>
         </main>
+        </div>
         </div>
 
   );
