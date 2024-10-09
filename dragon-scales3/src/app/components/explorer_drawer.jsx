dbd59c9179dac0956e7e13881fccfdc6a0a51b46
marcosf2 <marcosf2@illinois.edu>
2024-10-08 22:47:54 -0500

    Added tree that gets populated with the items inside notebooks

--- dragon-scales3/src/app/components/explorer_drawer.jsx
+++ dragon-scales3/src/app/components/explorer_drawer.jsx
@@ -1,14 +1,31 @@
+// TODO: Only 1 thing across all of the trees should be selected at a time. If an item is not selected and the user clicks it once, it should not expand but instead scroll there
+
 "use client";
 import { useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { Box, Typography, Drawer, Divider, Accordion, AccordionActions, AccordionSummary } from '@mui/material';
+import { Box, Typography, Drawer, Divider, Accordion, AccordionDetails, AccordionActions, AccordionSummary } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 
 const drawerWidth = 240;
 
 async function getLibraryStructure(id) {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/entities?ID=${id}`);
     return await res.json();
+}
+
+function createTreeStructure(item) {
+    // The first item sent to this function will not be included in the return
+    let ret = [];
+    if (item.children && item.children.length > 0) {
+        ret = item.children.map(child => ({
+            id: child.id,
+            label: child.name,
+            children: createTreeStructure(child)
+        }));
+    } 
+    return ret;
 }
 
 
@@ -20,12 +37,10 @@ const DrawerContent = styled('div')(({ theme }) => ({
 export default function ExplorerDrawer({open, name, id}) {
 
     const [libraryStructure, setLibraryStructure] = useState([]);
+    const [lastClickedItem, setLastClickedItem] = useState(null);
 
     useEffect(() => {
-        console.log(name, id);
         getLibraryStructure(id).then(data => {
-            console.log("Here comes the data");
-            console.log(data);
             setLibraryStructure(data);
         });
     }, []);
@@ -54,6 +69,10 @@ export default function ExplorerDrawer({open, name, id}) {
                         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                         {child.name}
                         </AccordionSummary>
+                        <AccordionDetails>
+                            <RichTreeView items={createTreeStructure(child)}
+                            onItemClick={(event, itemId) => setLastClickedItem(itemId)} />
+                        </AccordionDetails>
                     </Accordion>
                 ))}
             </DrawerContent>
