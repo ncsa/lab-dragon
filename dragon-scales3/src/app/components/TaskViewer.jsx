033d9c2d9c3b3b942b1285fbde73620726d00592
marcosf2 <marcosf2@illinois.edu>
2024-10-09 22:40:53 -0500

    Added content blocks to taskViewer

--- dragon-scales3/src/app/components/TaskViewer.jsx
+++ dragon-scales3/src/app/components/TaskViewer.jsx
@@ -1,7 +1,12 @@
 "use client"
 
+import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
 import {Typography, Paper, Stack, Breadcrumbs} from "@mui/material";
+
+import StepViewer from "./StepViewer";
+import { getEntity } from "@/app/utils";
+
 
 const StyledTaskPaper = styled(Paper)(({ theme }) => ({
     position: 'relative',
@@ -10,19 +15,29 @@ const StyledTaskPaper = styled(Paper)(({ theme }) => ({
     backgroundColor: "#FFFFFF",
     width: '90%',
     height: '90%',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
 }))
 
 const StyledTaskTittleTypography  = styled(Typography)(({ theme }) => ({
     position: 'relative',
     fontWeight: 'bold',
     fontSize: theme.typography.h4.fontSize,
-paddingLeft: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
 }))
 
 
 export default function TaskViewer({ taskEntity, breadcrumbsText }) {
 
-    console.log(breadcrumbsText)
+    const [steps, setSteps] = useState([]);
+
+    useEffect(() => {
+        Promise.all(taskEntity.children.map(child => getEntity(child))).then(steps => {
+            const newSteps = steps.map(s => JSON.parse(s));
+            setSteps(newSteps);
+        })
+    }, [taskEntity])
+
     return (
         <StyledTaskPaper>
             <Stack flexGrow={1} spacing={2} direction='column'>
@@ -36,11 +51,14 @@ export default function TaskViewer({ taskEntity, breadcrumbsText }) {
                 <StyledTaskTittleTypography>
                     {taskEntity.name}
                 </StyledTaskTittleTypography>
+                <Stack flexGrow={1} spacing={2} direction='column' paddingLeft={2}>
+                    {steps.map(step => (
+                        <StepViewer key={step.id} stepEntity={step} />
+                    ))}
+                </Stack>
             </Stack>
         </StyledTaskPaper>
     )
-
-
 }
 
 
